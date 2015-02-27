5d615fc7cb742309c448dddc01ff35eaeab65d9f
CIO <cio@JLdeiMac.AirDream>
2015-02-27 08:44:53 +0800

    Initial Commit

---
+++ Communication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Communication
+//
+//  Created by CIO on 15/2/27.
+//  Copyright (c) 2015年 JL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Communication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Communication
+//
+//  Created by CIO on 15/2/27.
+//  Copyright (c) 2015年 JL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
